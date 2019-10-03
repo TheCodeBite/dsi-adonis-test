75a9d459079a8e18fcf0de74d2c15a8dc38b9592
TheCodeBite <kamh.1996.hmak@gmail.com>
2019-10-03 10:57:06 -0500

    migracion

--- api/app/Controllers/Http/UserController.js
+++ api/app/Controllers/Http/UserController.js
@@ -18,9 +18,7 @@ class UserController {
 
     async login({ request, auth }){
         const { email , password } = request.all();
-
         const token = await auth.attempt(email, password);
-
         return token
     }
 }
